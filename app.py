8bf43b53d2be33c2d1559a72be130392f6a1818f
magnusAchor <rresha160@gmail.com>
2024-01-19 21:44:02 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  # Use your preferred database
+app.config['SECRET_KEY'] = 'your_secret_key'  # Change this to a secure key
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+# User Model
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+# Routes
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and password == user.password:
+            login_user(user)
+            return redirect(url_for('dashboard'))
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    with app.app_context():
+        # Create the database tables if they don't exist
+        db.create_all()
+
+    # Add your scraping logic here
+    scraped_data = ["Scraped Data 1", "Scraped Data 2", "Scraped Data 3"]
+    return render_template('dashboard.html', data=scraped_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
